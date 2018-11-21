73e2b49abefb6f0ef13674eb299de3e8b9e4a6d6
Robin TESSIER <robintess@hotmail.com>
2018-11-21 17:05:00 +0100

    rails yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+Restaurant.create([{ name: 'Macdo', address: 'Paris', category: 'french' }, { name: 'Quick', address: 'Paris', category: 'french' }, { name: '5guys', address: 'Paris', category: 'french' }, { name: 'Le camion qui fume', address: 'Paris', category: 'french' }, { name: 'Big Fernand', address: 'Paris', category: 'french' }])
+
+puts 'Finished!'
